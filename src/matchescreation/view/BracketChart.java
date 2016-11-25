bcf384232f0798919d8735f72a2059339e32a6b4
Emilia <emi.miod@gmail.com>
2016-11-25 11:47:47 +0100

    fixed non updated content height in binding

--- src/matchescreation/view/BracketChart.java
+++ src/matchescreation/view/BracketChart.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ChangeListener;
@@ -36,82 +37,75 @@ import matchescreation.Node;
  * @author Emilia
  */
 public class BracketChart extends Region {
-    
+
     private ArrayList<Node> matches;
     private Node current;
-    
+
     private int vert_gap;
     private int match_width;
 
-
     BracketChart(ArrayList<Node> matches, Node current) {
         this.matches = matches;
-        this.current = current;      
+        this.current = current;
         this.setId("bracket-chart");
-        
+
         vert_gap = 70;
         match_width = 220;
-        
+
         initBracket();
     }
-    
-    private void initBracket(){
+
+    private void initBracket() {
         int lvl = matches.get(0).getChartLvl();
         int cur_lvl = lvl;
         int m_amount = matches.size();
         int j = 0;
-        
-        
-        
-        
-        
-        for(int i = 0; i< m_amount;i++){
-            if(cur_lvl>matches.get(i).getChartLvl()){
-                cur_lvl = matches.get(i).getChartLvl();
-                j=0;
-            }
-            int tmp = lvl-cur_lvl;
-            int posX = (tmp)*300+30; //horizontal distance
-            int posY = (int) (Math.pow(2,tmp)*vert_gap + j++*2*vert_gap*(Math.pow(2, tmp))); // vertical distance + top shift
 
-            if(matches.get(i).equals(current))
+        for (int i = 0; i < m_amount; i++) {
+            if (cur_lvl > matches.get(i).getChartLvl()) {
+                cur_lvl = matches.get(i).getChartLvl();
+                j = 0;
+            }
+            int tmp = lvl - cur_lvl;
+            int posX = (tmp) * 300 + 30; //horizontal distance
+            int posY = (int) (Math.pow(2, tmp) * vert_gap + j++ * 2 * vert_gap * (Math.pow(2, tmp))); // vertical distance + top shift
+
+            if (matches.get(i).equals(current)) {
                 this.addCurrMatch(matches.get(i), posX, posY);
-            else    
+            } else {
                 this.addMatch(matches.get(i), posX, posY);
-       
-            if(cur_lvl<lvl)
-                addJoining(posX, posY, (int) ((Math.pow(2, tmp-1))*vert_gap));
+            }
+
+            if (cur_lvl < lvl) {
+                addJoining(posX, posY, (int) ((Math.pow(2, tmp - 1)) * vert_gap));
+            }
         }
-        
-        
-        
-        addDottedLine(lvl*300+30, (int) (Math.pow(2,lvl)*vert_gap));
-        
+
+        addDottedLine(lvl * 300 + 30, (int) (Math.pow(2, lvl) * vert_gap));
+
         addLvlNames();
-        
+
     }
-    
-    private void addMatch(Node match, int posX, int posY){
-        
+
+    private void addMatch(Node match, int posX, int posY) {
+
         //prepare names
         String akaName = "-";
         String akaClub = "-";
-        if(match.getAka() != null && match.getAka().getAthlete() != null){
+        if (match.getAka() != null && match.getAka().getAthlete() != null) {
             akaName = match.getAka().getAthlete().getFullName();
             akaClub = match.getAka().getAthlete().getClub();
         }
         String akaScore = (match.getScoreAka() != null) ? match.getScoreAka().toString() : "";
-        
+
         String shiroName = "-";
         String shiroClub = "-";
-        if(match.getShiro() != null && match.getShiro().getAthlete() != null){
+        if (match.getShiro() != null && match.getShiro().getAthlete() != null) {
             shiroName = match.getShiro().getAthlete().getFullName();
             shiroClub = match.getShiro().getAthlete().getClub();
         }
         String shiroScore = (match.getScoreShiro() != null) ? match.getScoreShiro().toString() : "";
-        
-        
-        
+
         //distances
         int distY = 50;
         int distClub = 20;
@@ -119,229 +113,194 @@ public class BracketChart extends Region {
         int icoMargin = 10;
         int icoSize = 20;
         int textShift = 15;
-        
-       //competitor color icons
+
+        //competitor color icons
         ImageView akaIco = new ImageView("matchescreation/view/images/aka.png");
         akaIco.setFitWidth(icoSize);
         akaIco.setFitHeight(icoSize);
         akaIco.relocate(posX, posY);
         ImageView shiroIco = new ImageView("matchescreation/view/images/shiro.png");
         shiroIco.setFitWidth(icoSize);
-        shiroIco.setFitHeight(icoSize);    
-        shiroIco.relocate(posX, posY+distY);
-        
+        shiroIco.setFitHeight(icoSize);
+        shiroIco.relocate(posX, posY + distY);
+
         //setFill(Color.rgb(222, 222, 222)); //text color
         Label a1 = new Label(akaName);
-        a1.relocate(posX + icoSize + icoMargin, posY-textShift);
+        a1.relocate(posX + icoSize + icoMargin, posY - textShift);
         Label a2 = new Label(akaClub);
-        a2.relocate(posX + icoSize + icoMargin, posY+distClub-textShift);
+        a2.relocate(posX + icoSize + icoMargin, posY + distClub - textShift);
         a2.getStyleClass().add("club");
         Label a3 = new Label(akaScore);
-        a3.relocate(posX+distScore, posY+0-textShift+5);
+        a3.relocate(posX + distScore, posY + 0 - textShift + 5);
         a3.getStyleClass().add("score");
-        
+
         Label s1 = new Label(shiroName);
-        s1.relocate(posX + icoSize + icoMargin, posY+distY-textShift);
+        s1.relocate(posX + icoSize + icoMargin, posY + distY - textShift);
         Label s2 = new Label(shiroClub);
-        s2.relocate(posX + icoSize + icoMargin, posY+distClub+distY-textShift);
+        s2.relocate(posX + icoSize + icoMargin, posY + distClub + distY - textShift);
         s2.getStyleClass().add("club");
         Label s3 = new Label(shiroScore);
-        s3.relocate(posX+distScore, posY+distY-textShift+5);
+        s3.relocate(posX + distScore, posY + distY - textShift + 5);
         s3.getStyleClass().add("score");
-        
-        Line l = new Line(posX, posY+distY-15, posX+distScore+20, posY+distY-15);
+
+        Line l = new Line(posX, posY + distY - 15, posX + distScore + 20, posY + distY - 15);
         l.getStyleClass().add("line");
-        
+
         //create match region and add it to the chart
         Pane m = new Pane();
-        m.getChildren().setAll(akaIco,shiroIco, a1,a2,a3, s1,s2,s3, l);
+        m.getChildren().setAll(akaIco, shiroIco, a1, a2, a3, s1, s2, s3, l);
         this.getChildren().addAll(m);
-        
+
     }
-    
-    private void addCurrMatch(Node match, int posX, int posY){
-        Rectangle rect = new Rectangle(posX-15, posY-15, match_width+50, 100);
+
+    private void addCurrMatch(Node match, int posX, int posY) {
+        Rectangle rect = new Rectangle(posX - 15, posY - 15, match_width + 50, 100);
         rect.getStyleClass().add("current-match");
-        
+
         Label l = new Label(Dictionary.getString("current-match").toUpperCase());
-        l.relocate(posX-15, posY-35);
+        l.relocate(posX - 15, posY - 35);
         l.getStyleClass().add("current-match-text");
-        l.setStyle("-fx-min-width: " + (match_width+50) + ";");
-        
+        l.setStyle("-fx-min-width: " + (match_width + 50) + ";");
+
         this.getChildren().addAll(rect, l);
-        
-        addMatch(match,posX,posY);
+
+        addMatch(match, posX, posY);
     }
-    
-    private void addJoining(int posX, int posY, int height){
+
+    private void addJoining(int posX, int posY, int height) {
         int width = 20;
         int dist = height;
-        
-        posX-= width+10;
-        posY+= 35;
-        
-        Line l_top = new Line(posX-width, posY-dist, posX, posY-dist);
+
+        posX -= width + 10;
+        posY += 35;
+
+        Line l_top = new Line(posX - width, posY - dist, posX, posY - dist);
         l_top.getStyleClass().add("line");
-        
-        Line l_bottom = new Line(posX-width, posY+dist, posX, posY+dist);
+
+        Line l_bottom = new Line(posX - width, posY + dist, posX, posY + dist);
         l_bottom.getStyleClass().add("line");
-        
-        Line l_middle = new Line(posX, posY, posX+width, posY);
+
+        Line l_middle = new Line(posX, posY, posX + width, posY);
         l_middle.getStyleClass().add("line");
-        
-        Line l_vert = new Line(posX, posY-dist, posX, posY+dist);
+
+        Line l_vert = new Line(posX, posY - dist, posX, posY + dist);
         l_vert.getStyleClass().add("line");
-        
+
         this.getChildren().addAll(l_top, l_bottom, l_middle, l_vert);
     }
-    
+
     private Pane lvlNames = new Pane();
-    
-    private void addLvlNames(){
+
+    private void addLvlNames() {
         int i = matches.get(0).getChartLvl();
-        int j=0;
-        
+        int j = 0;
+
         ArrayList<Label> labels = new ArrayList<>();
-        for(;i>=0;i--){
+        for (; i >= 0; i--) {
             Label l = new Label(Chart.getLvlToString(i).toUpperCase());
-            l.relocate(j++*300+30, 10);
+            l.relocate(j++ * 300 + 30, 10);
             l.getStyleClass().add("lvl");
-            l.getStyleClass().add("lvl-"+i);
-            l.setStyle("-fx-min-width: " + (match_width+50) + ";");
+            l.getStyleClass().add("lvl-" + i);
+            l.setStyle("-fx-min-width: " + (match_width + 50) + ";");
             labels.add(l);
         }
-        
+
         lvlNames = new Pane();
         lvlNames.getChildren().addAll(labels);
         this.getChildren().addAll(lvlNames);
-        
+
     }
-    
-    private void addDottedLine(int posX, int posY){
-        Line l = new Line(30, posY+35, posX-10, posY+35);
+
+    private void addDottedLine(int posX, int posY) {
+        Line l = new Line(30, posY + 35, posX - 10, posY + 35);
         l.getStyleClass().add("line");
         l.getStyleClass().add("dotted-line");
         l.getStrokeDashArray().addAll(30d, 30d);
-        
+
         this.getChildren().add(l);
     }
 
-    
     /**
      * Sets names of levels for the bracket chart vertically fixed
-     * @param bracketScrollPane 
+     *
+     * @param bracketScrollPane
      */
     void setScrollFixed(ScrollPane bracketScrollPane, Group scrollContent, StackPane zoomGroup) {
         zoomGroup.setMaxHeight(zoomGroup.getHeight());
+
+        DoubleProperty scrollVvalue = bracketScrollPane.vvalueProperty();
+        DoubleProperty contentHeight = new SimpleDoubleProperty();
         
-        
-        
-        // Allow horizontal scrolling of fixed element:
-        // here we bind circle Y position
-        lvlNames.layoutYProperty().bind(
-                // to vertical scroll shift (which ranges from 0 to 1)
-                bracketScrollPane.vvalueProperty()
-                    // multiplied by (scrollableAreaHeight - visibleViewportHeight)
-                    .multiply(
-                        new ScrollPaneContentHeightBinding(scrollContent)
-                                .multiply(new ZoomBinding(zoomGroup))
-                            .subtract(
-                                new ScrollPaneViewPortHeightBinding(bracketScrollPane))
-                    
-                    ).divide(new ZoomBinding(zoomGroup))
-                    
+        scrollContent.boundsInLocalProperty().addListener(
+                (ObservableValue<? extends Bounds> observableValue, Bounds bounds, Bounds bounds2)
+                -> {contentHeight.set(bounds2.getHeight());}
         );
-         zoomGroup.setPrefSize(
-      lvlNames.getParent().getParent().getLayoutX(),
-      lvlNames.getParent().getParent().getLayoutY()
-    );
+
+        // fix lvlNames vertically
+        lvlNames.layoutYProperty().bind(
+                // to vertical scroll shift <0;1>
+                scrollVvalue
+                        // multiplied by (scrollableAreaHeight - visibleViewportHeight)
+                        .multiply(
+                                contentHeight
+                                        .subtract(new ViewportHeightBinding(bracketScrollPane))
+                        ).divide(new ZoomBinding(zoomGroup))
+        );
+
         
-//        bracketScrollPane.vvalueProperty().addListener( (observable, oldValue, newValue) -> {
-//            updateFixedPos();
-//        });        
-//        zoomGroup.scaleXProperty().addListener( (observable, oldValue, newValue) -> {
-//            updateFixedPos();
+        
+//        lvlNames.layoutYProperty().addListener((observable, oldValue, newValue) -> {
+//            System.out.println("pos" + lvlNames.getLayoutY()
+//                    + "\tScale " + zoomGroup.getScaleY()
+//                    + "\ttimes  " + bracketScrollPane.getVvalue()
+//                    + "\ttimes (" + bracketScrollPane.getContent().getBoundsInLocal().getHeight()
+//                    + "\t- " + bracketScrollPane.getBoundsInLocal().getHeight()
+//                    + "\t/" + bracketScrollPane.getHeight()
+//                    + "\tb " + bracketScrollPane.getViewportBounds().getHeight()
+//                    + ")\t=" + (bracketScrollPane.getVvalue() * (bracketScrollPane.getContent().getBoundsInLocal().getHeight() - bracketScrollPane.getViewportBounds().getHeight())));
 //        });
 
-
-        lvlNames.layoutYProperty().addListener( (observable, oldValue, newValue) -> {
-            System.out.println("pos"+lvlNames.getLayoutY() 
-                    + "\tScale "+zoomGroup.getScaleY()
-                    +"\ttimes  "+bracketScrollPane.getVvalue()
-                    +"\ttimes ("+bracketScrollPane.getContent().getBoundsInLocal().getHeight()
-                    +"\t- " + bracketScrollPane.getBoundsInLocal().getHeight()
-                    +"\t/"+ bracketScrollPane.getHeight()
-                    +"\tb "+bracketScrollPane.getViewportBounds().getHeight()
-                    +")\t="+(bracketScrollPane.getVvalue()*(bracketScrollPane.getContent().getBoundsInLocal().getHeight()-bracketScrollPane.getViewportBounds().getHeight())));
-        });
-        
         zoomGroup.setMinSize(230.0, 230.0);
-      //  zoomGroup.setPrefSize(USE_PREF_SIZE, USE_PREF_SIZE);
-   // zoomGroup.setPrefSize(20,20);
-        
+
+        //
+        //not sure yet if this will have to be 
+        zoomGroup.setPrefSize(
+                lvlNames.getParent().getParent().getLayoutX(),
+                lvlNames.getParent().getParent().getLayoutY()
+        );
         bracketScrollPane.viewportBoundsProperty().addListener(
-  new ChangeListener<Bounds>() {
-  @Override 
-  public void changed(ObservableValue<? extends Bounds> observableValue, Bounds oldBounds, Bounds newBounds) {
-    zoomGroup.setPrefSize(
-      lvlNames.getParent().getParent().getLayoutX(),
-      lvlNames.getParent().getParent().getLayoutY()
-    );
-  }
-  });
+                new ChangeListener<Bounds>() {
+            @Override
+            public void changed(ObservableValue<? extends Bounds> observableValue, Bounds oldBounds, Bounds newBounds) {
+                zoomGroup.setPrefSize(
+                        lvlNames.getParent().getParent().getLayoutX(),
+                        lvlNames.getParent().getParent().getLayoutY()
+                );
+            }
+        });
 
-         
-        
     }
-    
-//    private void updateFixedPos(ScrollPane sp, Group g){
-//        lvlNames.setLayoutX(sp.vvalueProperty()
-//                    // multiplied by (scrollableAreaHeight - visibleViewportHeight)
-//                    .multiply(
-//                        lvlNames.heightProperty()
-//                            .subtract(
-//                                new ScrollPaneViewPortHeightBinding(sp))
-//                    
-//                    ).multiply(g.getScaleY()));
-//    }
-    
-     private static class ScrollPaneContentHeightBinding extends DoubleBinding {
 
-        private final Group content;
-        //private final DoubleProperty contentHeight;
-
-        public ScrollPaneContentHeightBinding(Group content) {
-            this.content = content;
-         //   contentHeight = new SimpleDoubleProperty(root.getContent().getBoundsInLocal().getHeight());
-       //     super.bind(content.boundsInLocalProperty());
-        }
-
-        @Override
-        protected double computeValue() {
-            System.out.println("contentHeight "+content.getBoundsInLocal().getHeight());         
-            return content.getBoundsInLocal().getHeight();
-        }
-    }
-      private static class ScrollPaneViewPortHeightBinding extends DoubleBinding {
+    private static class ViewportHeightBinding extends DoubleBinding {
 
         private final ScrollPane root;
 
-        public ScrollPaneViewPortHeightBinding(ScrollPane root) {
+        public ViewportHeightBinding(ScrollPane root) {
             this.root = root;
             super.bind(root.viewportBoundsProperty());
         }
 
         @Override
         protected double computeValue() {
-            System.out.println("viewportHeight "+root.getViewportBounds().getHeight());         
+            //System.out.println("viewportHeight "+root.getViewportBounds().getHeight());         
             return root.getViewportBounds().getHeight();
         }
     }
-     
-        private static class ZoomBinding extends DoubleBinding {
 
-         private final StackPane zoomGroup;
-      
+    private static class ZoomBinding extends DoubleBinding {
+
+        private final StackPane zoomGroup;
+
         public ZoomBinding(StackPane zoomGroup) {
             this.zoomGroup = zoomGroup;
             super.bind(zoomGroup.scaleYProperty());
@@ -349,7 +308,7 @@ public class BracketChart extends Region {
 
         @Override
         protected double computeValue() {
-            System.out.println("zoom "+zoomGroup.getScaleY());         
+            //System.out.println("zoom "+zoomGroup.getScaleY());         
             return zoomGroup.getScaleY();
         }
     }
